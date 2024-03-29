04ff6805474118241ecdb9c1a7ee23a9b0d1bf25
unknown <Heryberthus029@gmail.com>
2024-03-29 22:21:51 +0700

    MR BIG DICK

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'Kontol']);
+Route::get('/taufik', [HomeController::class, 'Pepek']);
+Route::get('/hery', [HomeController::class, 'silit']);
